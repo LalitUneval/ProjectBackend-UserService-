4c6c744065a29995b004febc909db8f9dcf660f2
LALIT UNEVAL <unevallalit6499@gmail.com>
2026-04-17 17:24:04 +0530

    Fix error v2

--- src/main/java/com/lalit/userservice/DTO/UserProfileResponse.java
+++ src/main/java/com/lalit/userservice/DTO/UserProfileResponse.java
@@ -1,5 +1,6 @@
 package com.lalit.userservice.DTO;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -27,7 +28,9 @@ public class UserProfileResponse {
 
     private String phoneNumber;
 
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime createdAt;
 
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime updatedAt;
 }
